cc860db61203d96c39e5284a95ecc2ef27446a46
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-09-16 21:57:17 +0430

    Spring Core Spring DI Spring Controller Spring Thymeleaf

--- src/main/kotlin/org/yastech/am_golampoor_ums/controller/MainController.kt
+++ src/main/kotlin/org/yastech/am_golampoor_ums/controller/MainController.kt
@@ -1,26 +1,36 @@
 package org.yastech.am_golampoor_ums.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
+import org.yastech.am_golampoor_ums.service.UserService
 
 @Controller
 class MainController
+(
+    private var userService: UserService
+)
 {
     @GetMapping("/")
-    fun homePage(): String
+    fun homePage(@RequestParam(required = false) name: String?, model: Model): String
     {
+        model.addAttribute("name", name)
         return "index"
     }
 
     @GetMapping("/about")
-    fun aboutPage(): String
+    fun aboutPage(@RequestParam(required = false) name: String?, model: Model): String
     {
+        model.addAttribute("name", name)
         return "about"
     }
 
     @GetMapping("/contact")
     fun contactPage(): String
     {
+        userService.add()
         return "contact"
     }
 }
---
+++ src/main/kotlin/org/yastech/am_golampoor_ums/service/UserService.kt
@@ -0,0 +1,22 @@
+package org.yastech.am_golampoor_ums.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class UserService
+{
+    fun add()
+    {
+        println("add")
+    }
+
+    fun delete()
+    {
+        println("delete")
+    }
+
+    fun update()
+    {
+        println("update")
+    }
+}
